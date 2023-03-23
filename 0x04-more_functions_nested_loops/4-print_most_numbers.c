2d53fa147b1f2d5d8daaec953c5949fb86005a32
billwk254 <billkihungu51@gmail.com>
2023-03-23 20:39:57 +0000

    Print from 0 to 9 followed by a new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ *
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+  int c = 48;
+
+  while (c < 58)
+    {
+      if (!(c == '2' || c == '4'))
+	_putchar(c);
+      c++;
+    }
+  _putchar('\n');
+}
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ *
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+int c = 48;
+
+while (c < 58)
+{
+if (!(c == '2' || c == '4'))
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
